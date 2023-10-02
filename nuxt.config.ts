cf309037edf309afd56d747fea599b2d79a18470
Fathur Rahman <oman.fathur.rahman.nur@gmail.com>
2023-10-02 17:09:50 +0700

    disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/image',
     'nuxt-icon',
     '@vueuse/nuxt',
-    '@vexip-ui/nuxt',
+    '@vexip-ui/nuxt'
   ],
+  
 })
