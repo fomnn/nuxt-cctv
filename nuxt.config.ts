20bc848c0e034ccecc2dc88aa0c2c5fa79411ec8
Fathur Rahman N <95164774+omanfathurrahman@users.noreply.github.com>
2023-10-17 12:31:34 +0700

    hapus beberapa page
    hapus page:
    - keamanan-akun
    - pengaturan-tambahan

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  
   // devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
@@ -39,5 +40,6 @@ export default defineNuxtConfig({
     ],
     defaultLocale: 'id-ID',
     
-  }
+  },
+  
 })
